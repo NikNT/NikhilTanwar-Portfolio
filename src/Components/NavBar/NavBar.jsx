8b63f4211b16a7ab0146744eb9bd97bd5a0970f4
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-07-03 18:55:55 -0400

    2.1 Responsive Layout - Mobile - Initial Test Design

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import styles from "./NavBar.module.css";
 import darkmode from "../../Assets/Dark Mode Switch.png";
+import hamburger from "../../Assets/hamburger.png";
 
 const NavBar = () => {
   return (
-    <header>
-      <nav className={styles.nav}>
-        <ul className={styles.nav_links}>
-          <li>About Me</li>
-          <li>My Projects</li>
-          <li>Let's Connect</li>
-        </ul>
-        <img
-          src={darkmode}
-          alt="Dark Mode Switch"
-          className={styles.nav_darkmode}
-        />
-      </nav>
-    </header>
+    <>
+      <header>
+        <nav className={styles.nav}>
+          <ul className={styles.nav_links}>
+            <li className={styles.nav_links_li}>About Me</li>
+            <li className={styles.nav_links_li}>My Projects</li>
+            <li className={styles.nav_links_li}>Let's Connect</li>
+            <li className={styles.nav_links__hamburger}>
+              <img src={hamburger} alt="Hamburger Menu" />
+            </li>
+          </ul>
+          <img
+            src={darkmode}
+            alt="Dark Mode Switch"
+            className={styles.nav_darkmode}
+          />
+        </nav>
+      </header>
+    </>
   );
 };
 
