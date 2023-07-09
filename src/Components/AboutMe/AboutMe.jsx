4547dbfef49c6260cd3c75220a939fecc7038f7c
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-07-09 12:28:51 -0400

    2.13 AboutMe - New Cards Added | DarkMode for About Me - Fixed

--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -1,98 +1,13 @@
 import React from "react";
 import styles from "./AboutMe.module.css";
-import avatar from "../../Assets/avatar.png";
-import darkAvatar from "../../Assets/avatar-dark.png";
-import book from "../../Assets/book.gif";
-import developer from "../../Assets/developer-light.gif";
+import Cards from "../Cards/Cards";
 
 const AboutMe = ({ mode }) => {
   return (
     <section className={styles.aboutme}>
       <h4>About Me</h4>
-      <div className={styles.cardContainer}>
-        <div className={styles.aboutme_cards}>
-          <div className={styles.aboutme_card}>
-            <div className={styles.aboutme_card_title}>
-              <img
-                className={styles.aboutMe_card_title_img}
-                src={developer}
-                alt="Developer"
-              />
-              <p>My Tech Stack</p>
-            </div>
-            <div className={styles.aboutme_card_body}>
-              <p>
-                HTML5
-                <br />
-                CSS3
-                <br />
-                JavaScript
-                <br />
-                TypeScript
-              </p>
-              <p>
-                ReactJS <br /> NextJS
-              </p>
-              <p>
-                Bootstrap <br />
-                TailwindCSS
-                <br />
-                SASS
-              </p>
-            </div>
-          </div>
-          <div className={styles.aboutme_card}>
-            <div className={styles.aboutme_card_title}>
-              <img
-                className={styles.aboutMe_card_title_img}
-                src={mode ? avatar : darkAvatar}
-                alt="Avatar"
-              />
-              <p>Me</p>
-            </div>
-            <div className={styles.aboutme_card_body}>
-              <p>Hi, I'm Nikhil</p>
-              <p>
-                A Self-taught
-                <br />
-                Pixel Nerdy
-                <br />
-                Front End Dev
-              </p>
-              <p>
-                I love ReactJS, XBox Series S,
-                <br />
-                and Pizza!
-              </p>
-              <p>
-                <em> I sculpt digital landscapes that enchant and inspire </em>
-              </p>
-            </div>
-          </div>
-          <div className={styles.aboutme_card}>
-            <div className={styles.aboutme_card_title}>
-              <img
-                className={styles.aboutMe_card_title_img}
-                src={book}
-                alt="Book"
-              />
-              <p>Education</p>
-            </div>
-            <div className={styles.aboutme_card_body}>
-              <p>
-                B.Tech. CSE <br /> Lovely Professional University
-                <br />
-                Punjab, India
-              </p>
-              <p>
-                PG Diploma <br /> Cloud Computing for Big Data Lambton College,
-                Toronto
-              </p>
-              <p>Meta Front End Developer Professional Certificate</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Cards mode={mode} />
+      {/* <a href="https://lordicon.com/">Icons by Lordicon.com</a> */}
     </section>
   );
 };
---
+++ src/Components/Cards/Cards.jsx
@@ -0,0 +1,183 @@
+import React from "react";
+import styles from "./Cards.module.css";
+import darkAvatar from "../../Assets/avatar-dark.png";
+import book from "../../Assets/book.gif";
+import developer from "../../Assets/developer-light.gif";
+import bookReader from "../../Assets/book-reader.gif";
+
+const Cards = () => {
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.aboutme_cards}>
+        <div className={styles.aboutme_card}>
+          <div className={styles.aboutme_card_title}>
+            <img
+              className={styles.aboutMe_card_title_img}
+              src={book}
+              alt="Book"
+            />
+            <p>Education</p>
+          </div>
+          <div className={styles.aboutme_card_body}>
+            <p>
+              B.Tech. CSE <br /> Lovely Professional University
+              <br />
+              Punjab, India
+            </p>
+            <p>
+              PG Diploma <br /> Cloud Computing for Big Data Lambton College,
+              Toronto
+            </p>
+            <p>Meta Front End Developer Professional Certificate</p>
+          </div>
+        </div>
+        <div className={styles.aboutme_card}>
+          <div className={styles.aboutme_card_title}>
+            <img
+              className={styles.aboutMe_card_title_img}
+              src={darkAvatar}
+              alt="Avatar"
+            />
+            <p>Me</p>
+          </div>
+          <div className={styles.aboutme_card_body}>
+            <p>Hi, I'm Nikhil</p>
+            <p>
+              A Self-taught
+              <br />
+              Pixel Nerdy
+              <br />
+              Front End Dev
+            </p>
+            <p>
+              I love ReactJS, XBox Series S,
+              <br />
+              and Pizza!
+            </p>
+            <p>
+              <em> I sculpt digital landscapes that enchant and inspire </em>
+            </p>
+          </div>
+        </div>
+        <div className={styles.aboutme_card}>
+          <div className={styles.aboutme_card_title}>
+            <img
+              className={styles.aboutMe_card_title_img}
+              src={developer}
+              alt="Developer"
+            />
+            <p>My Tech Stack</p>
+          </div>
+          <div className={styles.aboutme_card_body}>
+            <p>
+              HTML5
+              <br />
+              CSS3
+              <br />
+              JavaScript
+              <br />
+              TypeScript
+            </p>
+            <p>
+              ReactJS <br /> NextJS
+            </p>
+            <p>
+              Bootstrap <br />
+              TailwindCSS
+              <br />
+              SASS
+            </p>
+          </div>
+        </div>
+        <div className={styles.aboutme_card}>
+          <div className={styles.aboutme_card_title}>
+            <img
+              className={styles.aboutMe_card_title_img}
+              src={bookReader}
+              alt="Book Reader"
+            />
+            <p>Work Experience</p>
+          </div>
+          <div className={styles.aboutme_card_body}>
+            <p>
+              Front End Developer - 2 Years
+              <br />
+              <br />
+              <span className={styles.aboutme_card_body_author}>
+                Worked in a budding Ed-Tech Company in Punjab, India <br />
+                <br /> Collaborated with UI/UX Designers, Back End Developers to
+                develop and maintain mission critical business systems.
+                <br />
+                <br />
+                Gained first-hand professional experience related to Web Design
+                and Development.
+              </span>
+            </p>
+          </div>
+        </div>
+        <div className={styles.aboutme_card}>
+          <div className={styles.aboutme_card_title}>
+            <img
+              className={styles.aboutMe_card_title_img}
+              src={bookReader}
+              alt="Book Reader"
+            />
+            <p>Fav Reads</p>
+          </div>
+          <div className={styles.aboutme_card_body}>
+            <p>
+              Shoe Dog
+              <br />
+              <span className={styles.aboutme_card_body_author}>
+                {" "}
+                Phil Knight{" "}
+              </span>
+              <br />
+              <em className={styles.aboutme_card_body_quote}>
+                "Life is growth. You grow or you die."
+              </em>
+            </p>
+            <p>
+              Atomic Habits <br />{" "}
+              <span className={styles.aboutme_card_body_author}>
+                {" "}
+                James Clear{" "}
+              </span>{" "}
+              <br />{" "}
+              <em className={styles.aboutme_card_body_quote}>
+                "Every action you take is a vote for the type of person you wish
+                to become."
+              </em>
+            </p>
+            {/* <p>
+              Life's Amazing Secrets <br />
+              TailwindCSS
+              <br />
+              SASS
+            </p> */}
+          </div>
+        </div>
+        <div className={styles.aboutme_card}>
+          <div className={styles.aboutme_card_title}>
+            <img
+              className={styles.aboutMe_card_title_img}
+              src={bookReader}
+              alt="Book Reader"
+            />
+            <p>Fav Quote</p>
+          </div>
+          <div className={styles.aboutme_card_body}>
+            <p>
+              <em className={styles.aboutme_card_body_quote_fav}>
+                "One last drink and the bottle breaks, returning us to the dust
+                from whence we came"
+              </em>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
